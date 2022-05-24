1242ee6f5a4619a9fd6926ed5e5b5f01228b31cc
Alex Lamb <alexlamb104@gmail.com>
2022-05-23 23:57:29 -0600

    Worked through exercise to practice data binding in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'binding-practice';
+  selectedIndex: number = -1;
+
+  flops = [
+    "https://drive.google.com/uc?export=view&id=1jg9XbBTf7mwaJKl16Knvr7Exzal6G7q3", "https://drive.google.com/uc?export=view&id=1PBKsp92XH9MUMve3Ls7jdzU2r8H0JstH", "https://drive.google.com/uc?export=view&id=1wk7B5EvZ0qUs4YoNthLsf4i5rSbD0LiJ", "https://drive.google.com/uc?export=view&id=1Or5hNiU9RcJKFucNGWR1r6K0KC4fYNYZ"
+  ];
+
+  isSelected(i: number) {
+    return this.selectedIndex === i
+  }
+
+  voteFor(index: number) {
+    if(this.isSelected(index)){
+      this.selectedIndex = -1
+    }else {
+      this.selectedIndex = index
+    }
+  }
+
+  getButtonText(index : number) {
+    if(this.isSelected(index)){
+      return "Unvote"
+    } else{
+      return "Vote"
+    }
+  }
+
+  anySelected() {
+    return this.selectedIndex !== -1
+  }
 }
